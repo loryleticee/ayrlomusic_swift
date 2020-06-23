8302d6a9a1242805a21d17aacaf1e82ef4e54352
Lory LETICEE <loryleticee@gmail.com>
2020-06-24 00:10:07 +0200

    Initial Commit

---
+++ ayrlomusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ayrlomusic
+//
+//  Created by Lory LETICEE on 24/06/2020.
+//  Copyright © 2020 Lory LETICEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
