c8f12fe93999d5d017f84af922454344d507af1c
Lory LETICEE <loryleticee@gmail.com>
2020-06-24 11:20:32 +0200

    fix landing page constraints

--- ayrlomusic/ViewController.swift
+++ ayrlomusic/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var strLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //
+        
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        self.strLabel.text = "Ayrlomusic"
     }
 
-
+    
 }
 
